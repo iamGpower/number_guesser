0c29e2dd5c50d63309d6689195253b4f5c20b2aa
iamGP <iamgpower3@gmail.com>
2022-01-26 20:49:47 +0100

    basic logic for game implemented

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretValue = Math.trunc(Math.random() * 20) + 1;
+let playerScore = 20;
+document.querySelector('.number').textContent = secretValue;
+
+document.querySelector('.check').addEventListener('click', () => {
+	const guess = Number(document.querySelector('.guess').value);
+	if (!guess) {
+		document.querySelector('.message').textContent = '❌ No number entered';
+	} else if (guess === secretValue) {
+		document.querySelector('.message').textContent = '🎉 Correct Guess';
+	} else if (guess > secretValue) {
+		if (playerScore > 1) {
+			document.querySelector('.message').textContent = '📈 Guess too high!';
+			playerScore--;
+			document.querySelector('.score').textContent = playerScore;
+		} else {
+			document.querySelector('.message').textContent =
+				'😭 Sorry, you lost the game!';
+			document.querySelector('.score').textContent = 0;
+		}
+	} else if (guess < secretValue) {
+		if (playerScore > 1) {
+			document.querySelector('.message').textContent = '📉 Guess too low!';
+			playerScore--;
+			document.querySelector('.score').textContent = playerScore;
+		} else {
+			document.querySelector('.message').textContent =
+				'😭 Sorry, you lost the game!';
+			document.querySelector('.score').textContent = 0;
+		}
+	}
+});
